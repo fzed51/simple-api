ed04b09c2eef7416ad7b6fdd41661bd48e3cafc2
Fabien Sanchez <fzed51+git@gmail.com>
2020-01-07 18:04:20 +0100

    wip | start ControllerTestCase class

---
+++ tests/Functional/ControllerTestCase.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+/**
+ * User: Fabien Sanchez
+ * Date: 07/01/2020
+ * Time: 17:58
+ */
+
+namespace Tests\Functional;
+
+
+use Slim\Http\Request;
+
+class ControllerTestCase extends ActionTestCase
+{
+
+    protected function getRequestWithOwner(string $method, string $url, $body): Request
+    {
+
+    }
+
+}
