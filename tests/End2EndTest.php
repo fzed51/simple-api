aa45bf82da9a82d09f7ffd3c7e257e76e8fa32d7
Fabien Sanchez <fzed51+git@gmail.com>
2020-01-19 08:45:34 +0100

    feat | ajout des routes
    pb : update & delete

--- tests/End2EndTest.php
+++ tests/End2EndTest.php
@@ -13,16 +13,94 @@ use Tests\Functional\AppTestCase;
 class End2EndTest extends AppTestCase
 {
 
-    public function test_AjoutDUneRessource(): void
+    /**
+     * @throws \Throwable
+     */
+    public function test_LireLesRessources(): void
     {
         $response = $this->runApp(
             'GET',
-            'item',
+            '/item',
             [
                 'Authorization' => $this->getOwnerBearerToken()
             ]
         );
-        echo (string)$response->getBody();
+        $this->assertSuccessResponse($response);
+        $data = $this->getDataResponse($response);
+        $this->assertIsArray($data);
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function test_LireLesRessourcesQuandOnEstPasOwner(): void
+    {
+        $response = $this->runApp(
+            'GET',
+            '/item',
+            [
+                'Authorization' => 'Bearer 123ade-123ade-123ade-123ade'
+            ]
+        );
+        $this->assertErrorResponse($response);
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function test_AjouterUneRessource(): void
+    {
+        $nbItems = $this->dbCount('entity');
+        $response = $this->runApp(
+            'POST',
+            '/item',
+            [
+                'Authorization' => $this->getOwnerBearerToken()
+            ],
+            [
+                'foo' => 'bar'
+            ]
+        );
+        //echo (string)$response->getBody();
+        $this->assertSuccessResponse($response);
+        $afterNbItem = $this->dbCount('entity');
+        $this->assertEquals($nbItems + 1, $afterNbItem);
+    }
+
+    public function test_ModifierUneRessource(): void
+    {
+        $refItem = $this->addEntity(
+            $this->getOwner()->getRef(),
+            'item',
+            json_encode(['foo' => 'bar'])
+        );
+        $response = $this->runApp(
+            'POST',
+            "/item/$refItem",
+            [
+                'Authorization' => $this->getOwnerBearerToken()
+            ],
+            [
+                'foo' => 'baz'
+            ]
+        );
+        $this->assertSuccessResponse($response);
+    }
+
+    public function test_SupprimerUneRessource(): void
+    {
+        $refItem = $this->addEntity(
+            $this->getOwner()->getRef(),
+            'item',
+            json_encode(['foo' => 'bar'])
+        );
+        $response = $this->runApp(
+            'DELETE',
+            "/item/$refItem",
+            [
+                'Authorization' => $this->getOwnerBearerToken()
+            ]
+        );
         $this->assertSuccessResponse($response);
     }
 
