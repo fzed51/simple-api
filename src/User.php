8e9d773c6faa88578ebbc4347f6ac191026b1319
Fabien Sanchez <fzed51+git@gmail.com>
2020-05-28 13:24:53 +0200

    wip | creation de l'entite User
    Le User doit servir de support au owner d'entity.
    le Owner actuel change de nom et devient Client

---
+++ src/User.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * User: Fabien Sanchez
+ * Date: 28/05/2020
+ * Time: 11:23
+ */
+
+namespace App;
+
+
+class User
+{
+    public function __construct($data)
+    {
+    }
+
+}
