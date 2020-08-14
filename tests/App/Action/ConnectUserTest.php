3ceda47935fe68a429c93541e120c124983664a0
Fabien Sanchez <fzed51+git@gmail.com>
2020-08-15 01:18:24 +0200

    test | fix ConnectUser test

--- tests/App/Action/ConnectUserTest.php
+++ tests/App/Action/ConnectUserTest.php
@@ -17,8 +17,9 @@ class ConnectUserTest extends ActionTestCase
 
     public function test__invoke()
     {
-        $user = $this->getNewUser();
         $connect = new ConnectUser($this->getPdo());
+        $connect->hydrateOwner($this->getOwner());
+        $user = json_encode($this->getNewUser());
         $session = $connect($user);
         self::assertInstanceOf(Session::class, $session);
     }
