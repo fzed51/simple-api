0abdae6179ff37e8f42d3a470813fab5010837b5
Fabien Sanchez <fzed51+git@gmail.com>
2023-01-02 13:55:51 +0100

    🚀 mise en place definitive du logger

--- src/dependencies.php
+++ src/dependencies.php
@@ -2,8 +2,13 @@
 declare(strict_types=1);
 
 use DI\ContainerBuilder;
+use Monolog\Formatter\JsonFormatter;
+use Monolog\Handler\RotatingFileHandler;
 use Monolog\Handler\StreamHandler;
+use Monolog\Level;
 use Monolog\Logger;
+use Monolog\LogRecord;
+use Monolog\Processor\IntrospectionProcessor;
 use Monolog\Processor\UidProcessor;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
@@ -12,17 +17,35 @@ use SimpleApi\Settings\Settings;
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
         LoggerInterface::class => function (ContainerInterface $c) {
-            /** @var Settings $settings */
+        /** @var Settings $settings */
             $settings = $c->get(Settings::class);
+            if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+                $ipClient = $_SERVER['HTTP_CLIENT_IP'];
+            } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                $ipClient = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            } else {
+                $ipClient = $_SERVER['REMOTE_ADDR'] ?? 'unknown';
+            }
             $logger = new Logger($settings->logSetting->apiName);
-            $uiProcessor = new UidProcessor();
-            $webProcess = new Monolog\Processor\WebProcessor();
-            $interProcess = new Monolog\Processor\IntrospectionProcessor();
-            $logger->pushProcessor($uiProcessor);
-            $logger->pushProcessor($webProcess);
-            $logger->pushProcessor($interProcess);
-            $handler = new StreamHandler($settings->logSetting->path, $settings->logSetting->level);
-            $logger->pushHandler($handler);
+            $logPath = $settings->logSetting->path;
+            $logError = rtrim($logPath, '/') . '/events-' . (new DateTime())->format('Y-m-d') . '.log';
+            $logDebug = rtrim($logPath, '/') . '/events_json.log';
+            $logger->pushProcessor(new UidProcessor());
+            $logger->pushProcessor(new IntrospectionProcessor());
+            $logger->pushProcessor(static function (LogRecord $record) use ($ipClient): LogRecord {
+                $record['extra']['info'] = [
+                    'REQUEST_METHOD' => $_SERVER['REQUEST_METHOD'] ?? '?',
+                    'REQUEST_URI' => $_SERVER['REQUEST_URI'] ?? '?',
+                    'IP' => $ipClient,
+                    'USER_AGENT' => $_SERVER['HTTP_USER_AGENT'] ?? '?'
+                ];
+                return $record;
+            });
+            $steam = new StreamHandler($logError, Level::Warning);
+            $jsonSteam = new RotatingFileHandler($logDebug, 15, $settings->logSetting->level);
+            $jsonSteam->setFormatter(new JsonFormatter());
+            $logger->pushHandler($steam);
+            $logger->pushHandler($jsonSteam);
             return $logger;
         },
     ]);
