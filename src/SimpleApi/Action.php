65b48c0e6bb8918068a9d7183e86f3e98ebcfede
Fabien Sanchez <fzed51+git@gmail.com>
2023-03-07 21:39:45 +0100

    🚀 ajout de la gestion d'une valeur d'action dans la class Action

--- src/SimpleApi/Action.php
+++ src/SimpleApi/Action.php
@@ -3,14 +3,44 @@ declare(strict_types=1);
 
 namespace SimpleApi;
 
+use LogicException;
+
 /**
  * Class abstraite de base pour les actions
  */
 abstract class Action
 {
+
+    public bool $initializedValue = false;
+    public mixed $value;
+
     /**
      * Méthode de base permettant d'exécuter cette action
      * @return void
      */
     abstract public function execute():void;
+
+    public function getValue(): mixed
+    {
+        if (!$this->initializedValue) {
+            throw new LogicException("La valeur de " . static::class . " n'a pas été initialisée");
+        }
+        return $this->value;
+    }
+
+    /**
+     * @param mixed $value
+     * @return void
+     */
+    protected function setValue(mixed $value): void
+    {
+        $this->initializedValue = true;
+        $this->value = $value;
+    }
+
+    protected function clearValue(): void
+    {
+        $this->initializedValue = false;
+        $this->value = null;
+    }
 }
