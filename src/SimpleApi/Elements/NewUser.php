9e4465aff4c95aaaf1eceec5bc9871e6cc7616fe
Fabien Sanchez <fzed51+git@gmail.com>
2023-03-07 21:56:44 +0100

    ✨ rationalise les proprietes du NewUser

--- src/SimpleApi/Elements/NewUser.php
+++ src/SimpleApi/Elements/NewUser.php
@@ -10,40 +10,21 @@ class NewUser
 {
     /**
      * Constructeur de NewUser
-     * @param string $entity 10car
      * @param string $email 320car
      * @param string $pass 256car
-     * @param string $salt 36car
-     * @param bool $enable
-     * @param string $enableToken 36car
-     * @param string $enableLimit au format yyyy-mm-dd
      */
     public function __construct(
-        readonly public string $entity,
         readonly public string $email,
         readonly public string $pass,
-        readonly public string $salt,
-        readonly public bool   $enable,
-        readonly public string $enableToken,
-        readonly public string $enableLimit
     ) {
     }
 
     /**
-     * @param \SimpleApi\Elements\Entity $entity
      * @param array<string,mixed> $data
      * @return self
      */
-    public static function fromArray(Entity $entity, array $data)
+    public static function fromArray(array $data)
     {
-        return new self(
-            $entity->uuid,
-            $data['email'],
-            $data['pass'],
-            $data['salt'],
-            $data['enable'],
-            $data['enable_token'],
-            $data['enable_limit']
-        );
+        return new self($data['email'], $data['pass']);
     }
 }
